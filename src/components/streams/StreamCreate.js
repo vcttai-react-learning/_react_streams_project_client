278f3fe09bb7435662fbe0139235d4b2f1563262
vcttai <vcttai@gmail.com>
2019-05-22 18:41:32 +0700

    20 - Create stream and make stream reducers

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends React.Component
 {
@@ -30,8 +32,9 @@ class StreamCreate extends React.Component
         );
     }
 
-    onSubmit( formValues ) {
+    onSubmit = ( formValues ) => {
         console.log( formValues );
+        this.props.createStream(formValues);
     }
 
     render() {
@@ -73,7 +76,14 @@ const validate = (formValues) => {
     return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(
+    null,
+    {
+        createStream
+    }
+)(formWrapped);
